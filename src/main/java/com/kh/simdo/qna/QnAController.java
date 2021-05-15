cb089ba405ca1b023c701f0f2f6973a95e678311
season91 <choayoung91@naver.com>
2021-05-15 17:15:51 +0900

    qna 구현중

--- src/main/java/com/kh/simdo/qna/QnAController.java
+++ src/main/java/com/kh/simdo/qna/QnAController.java
@@ -2,10 +2,16 @@ package com.kh.simdo.qna;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping(value = "/qna")
 public class QnAController {
+
+    @GetMapping("/list")
+    public void qnaList(){
+
+    }
 }
